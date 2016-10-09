6757712da49c188f069ec9d2549f317666271205
Anne Fraysse <fraysse.anne@gmail.com>
2016-10-09 10:54:59 -0700

    practice java problems.

---
+++ practice_probs_I.java
@@ -0,0 +1,18 @@
+/* Use a scanner class to parse int types. Read input from stdin
+and specify the input stream as System.in */
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
